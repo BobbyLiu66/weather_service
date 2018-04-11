9eb8b93917235b16b475b7566e7ab153db194f54
BobbyLiu66 <bobbyliu45@gmail.com>
2018-04-11 16:56:40 +1200

    change check ip way

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const requestIp = require('request-ip');
 const weather = require('../src/service/weather');
 /* GET home page. */
 router.get('/weather', (req, res) => {
-    const ipAddress = req.connection.remoteAddress.replace("::ffff:","");
-    weather.currentWeatherDate(req,res,ipAddress);
+    const clientIp = requestIp.getClientIp(req);
+    weather.currentWeatherDate(req,res,clientIp);
 
 });
 
