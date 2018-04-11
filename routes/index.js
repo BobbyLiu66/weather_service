07e53a1ebf39bbab1804e0d9f19dcd27b7a21fdd
BobbyLiu66 <bobbyliu45@gmail.com>
2018-04-11 16:11:47 +1200

    test

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const weather = require('../src/service/weather');
 
 /* GET home page. */
 router.get('/weather', (req, res) => {
+    console.log(req.connection.remoteAddress)
     const ipAddress = req.connection.remoteAddress.replace("::ffff:","");
     const result = weather.currentWeatherDate(ipAddress);
     res.statusCode = 200;
